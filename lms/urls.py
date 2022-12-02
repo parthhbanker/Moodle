b9f67cbf49c3e72dccd2e8492f482780379ddd58
Puzer-03 <parthhbanker@gmail.com>
2022-12-02 12:31:46 +0530

    error pages

--- lms/urls.py
+++ lms/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+
     # default admin page
     path('admin/', include('admin.urls')),
     path('', include('admin.urls'))
